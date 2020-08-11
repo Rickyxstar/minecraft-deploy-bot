144610d89851a17c9fcf4e6d9755e379a6bbfba2
Ricky Martinez <rickmartinez4211@gmail.com>
2020-08-11 12:17:12 -0400

    Removed unsupported hyper link

--- index.ts
+++ index.ts
@@ -130,5 +130,5 @@ const help = async (message: MessageParser) => {
 }
 
 const info = async (message: MessageParser) => {
-    message.message.reply("\n**Mod pack:** MC Eternal \n**Version:** [1.3.7.1](https://www.curseforge.com/minecraft/modpacks/minecraft-eternal/download/3018921?client=y) \n**MC Version:** 1.12.2 \n**Server:** minecraft.critcola.com")
+    message.message.reply("\n**Mod pack:** MC Eternal \n**Version:** 1.3.7.1 \n**MC Version:** 1.12.2 \n**Server:** minecraft.critcola.com")
 }
